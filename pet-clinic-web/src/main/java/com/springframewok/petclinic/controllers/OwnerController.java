d5cf37fb2122524aafdced3d6b1ad0c692013e05
Rafal <grafal96@gmail.com>
2020-03-05 22:47:44 +0100

    Adding page and controller for owners and vets. Closes #6 and Closes #12

---
+++ pet-clinic-web/src/main/java/com/springframewok/petclinic/controllers/OwnerController.java
@@ -0,0 +1,16 @@
+package com.springframewok.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOfOwners(){
+
+        return "owners/index";
+    }
+
+}
---
+++ pet-clinic-web/src/main/java/com/springframewok/petclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package com.springframewok.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/vets")
+@Controller
+public class VetController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
