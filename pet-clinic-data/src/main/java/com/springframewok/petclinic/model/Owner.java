2767c205ac27608675c7bb5bd4d1fda98efdd4d6
Rafal <grafal96@gmail.com>
2020-03-13 21:51:21 +0100

    Added visits, updated owner with pet relationship. Closes #25

--- pet-clinic-data/src/main/java/com/springframewok/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframewok/petclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package com.springframewok.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pet;
+
+    public Set<Pet> getPet() {
+        return pet;
+    }
+
+    public void setPet(Set<Pet> pet) {
+        this.pet = pet;
+    }
 }
