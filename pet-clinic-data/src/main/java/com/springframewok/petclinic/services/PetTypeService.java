4be8a18abf09187ae55c05bba6ac173beb83b16b
Rafal <grafal96@gmail.com>
2020-03-14 23:26:55 +0100

    Added PetTypeService. Closes #28

---
+++ pet-clinic-data/src/main/java/com/springframewok/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframewok.petclinic.services;
+
+import com.springframewok.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/springframewok/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package com.springframewok.petclinic.services.map;
+
+import com.springframewok.petclinic.model.PetType;
+import com.springframewok.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService{
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
