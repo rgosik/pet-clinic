d016d679028937181546f15491b952ccddff86e7
Rafal <grafal96@gmail.com>
2020-03-14 23:35:15 +0100

    Added PetTypes to bootstrap. Closes #31

--- pet-clinic-web/src/main/java/com/springframewok/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframewok/petclinic/bootstrap/DataLoader.java
@@ -2,8 +2,10 @@ package com.springframewok.petclinic.bootstrap;
 
 
 import com.springframewok.petclinic.model.Owner;
+import com.springframewok.petclinic.model.PetType;
 import com.springframewok.petclinic.model.Vet;
 import com.springframewok.petclinic.services.OwnerService;
+import com.springframewok.petclinic.services.PetTypeService;
 import com.springframewok.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,14 +15,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog1");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat1");
+        PetType savedCatType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Bob");
