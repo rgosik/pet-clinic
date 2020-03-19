2589c54ab20422e2fb040b3185439c9177a4090f
Rafal <grafal96@gmail.com>
2020-03-19 16:37:33 +0100

    Updated Visit with JPA entity mappings. Closes #37

--- pet-clinic-data/src/main/java/com/springframewok/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springframewok/petclinic/model/Pet.java
@@ -1,10 +1,9 @@
 package com.springframewok.petclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -19,6 +18,9 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     private LocalDate birthDate;
 
@@ -44,6 +46,14 @@ public class Pet extends BaseEntity {
 
     public void setOwner(Owner owner) {
         this.owner = owner;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 
     public LocalDate getBirthDate() {
--- pet-clinic-data/src/main/java/com/springframewok/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/springframewok/petclinic/model/Visit.java
@@ -1,11 +1,20 @@
 package com.springframewok.petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit {
 
     private LocalDate date;
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
