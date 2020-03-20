cf21d3bf63bc225b291d0b263e3ecdb0e84e3485
Rafal <grafal96@gmail.com>
2020-03-20 20:22:40 +0100

    Added Visit Service and Visit Service Map impl. Closes #45

---
+++ pet-clinic-data/src/main/java/com/springframewok/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.springframewok.petclinic.services;
+
+import com.springframewok.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit> {
+}
---
+++ pet-clinic-data/src/main/java/com/springframewok/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,20 @@
+package com.springframewok.petclinic.services.map;
+
+import com.springframewok.petclinic.model.Visit;
+import com.springframewok.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit> implements VisitService {
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getId() == null || visit.getPet().getOwner() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+}
